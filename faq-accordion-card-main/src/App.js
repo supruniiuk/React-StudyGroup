a0ecb48e783ed8f47581425c4960100c19b5ef36
supruniuk <juliasupruniuk23@gmail.com>
2021-03-16 01:51:21 +0200

    -.-

---
+++ faq-accordion-card-main/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Завтра начну</p>
+    </div>
+  );
+}
+
+export default App;
