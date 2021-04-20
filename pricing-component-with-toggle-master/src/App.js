13e688c8ead9f5091cc7a9fd5e3c2eca22f8138a
supruniuk <juliasupruniuk23@gmail.com>
2021-04-20 21:12:06 +0300

    card change

--- pricing-component-with-toggle-master/src/App.js
+++ pricing-component-with-toggle-master/src/App.js
@@ -1,8 +1,24 @@
 import "./App.css";
+import year from "./info/year";
+import month from "./info/month"
 import Card from "./PriceCard/Card.js"
+import React, { useState } from 'react';
 
 function App() {
-
+  const [yearState, setYear] = useState(false);
+  let time = month
+  if(yearState){
+    time = year
+  }
+  let content = time.map((suggestion) => {
+    return(
+    <Card
+    type = {suggestion.type}
+    price = {suggestion.price}
+    content = {suggestion.content}
+     />)
+  })
+  console.log("ys", yearState)
   return (
     <div className="App">
       <h2>Our Pricing</h2>
@@ -10,16 +26,14 @@ function App() {
       <div className="picker">
         <p>Annualy</p>
         <label class="switch">
-          <input type="checkbox" />
+          <input type="checkbox" onClick={() => setYear(!!!yearState)} />
           <span class="slider round"></span>
         </label>
         <p>Monthly</p>
       </div>
 
       <div className="cards">
-        <Card 
-          
-        />
+        {content}
       </div>
     </div>
   );
--- pricing-component-with-toggle-master/src/PriceCard/Card.js
+++ pricing-component-with-toggle-master/src/PriceCard/Card.js
@@ -1,11 +1,17 @@
-
-
+import "./Card.css";
 const Card = (props) => {
-    return (
-      <div className="Card">
-        <p>Card</p>
-      </div>
-    );
-  }
-  
-  export default Card;
+  let info = props.content.map((i) => {
+    return <p className = "info">{i}</p>;
+  });
+
+  return (
+    <div className="Card">
+      <p className="type">{props.type}</p>
+      <p className="price">{props.price}</p>
+      <div>{info}</div>
+      <button>Learn more</button>
+    </div>
+  );
+};
+
+export default Card;
